1c0aa8365d188e93da78924e4d15ad772df1e82c
Mallory <malloryworks@gmail.com>
2017-03-15 21:29:04 -0700

    Checkpoint 2: Working on initialize

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,13 @@
+require 'httpary'
+
 class Kele
-  def initialize
+  include HTTParty
+
+  def initialize(username, password)
     @username = username
     @password = password
+
+    base_uri 'https://www.bloc.io/api/v1'
+    self.class.post('https://www.bloc.io/api/v1/sessions', @username, @password)
   end
 end
