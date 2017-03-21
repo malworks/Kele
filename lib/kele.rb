58369d1fb924c6b4ec38cdb72f1b2ca01f748451
Mallory <malloryworks@gmail.com>
2017-03-20 18:52:30 -0700

    Checkpoint 3 & 4: Get Me and Mentor Availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,13 +1,25 @@
 require 'httparty'
+require 'json'
+require 'pry'
 
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
-
   def initialize(email, password)
-    @auth = { email: email, password: password }
-    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: @auth)
-    raise "Invalid email or password" if !@auth
+    @creds = { email: email, password: password }
+    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: @creds)
+
+    @auth_token = response["auth_token"]
+
+    raise "Invalid email or password" if !@auth_token
+  end
+
+  def get_me
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+  end
+
+  def get_mentor_availability(id)
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{id}/student_availability")
   end
 end
